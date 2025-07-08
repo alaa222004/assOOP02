29823b93f639e2342d64831b789c5804f1850bf8
alaa samer <alaasamer222004@gmail.com>
2025-07-09 02:18:26 +0300

    Q1 solved

--- assOOP02/Program.cs
+++ assOOP02/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Xml.Linq;
 
 namespace assOOP02
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+// Design and implement a Class for the employees in a company:
+//Notes:
+//Employee is identified by an ID, Name, security level, salary, hire date and Gender.
+//We need to restrict the Gender field to be only M or F[Male or Female]
+//Assign the following security privileges to the employee(guest, Developer, secretary and DBA) in a form of Enum.
+//We want to provide the Employee Class to represent Employee data in a string Form(override ToString()), display
+//employee salary in a currency format. [Use String.Format() Function].
+
         }
-    }
+}
 }
