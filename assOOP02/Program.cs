e1e2116a46727707f029ce82d9a480ce2835abe6
alaa samer <alaasamer222004@gmail.com>
2025-07-10 15:03:54 +0300

    Q2 solved but not complete

--- assOOP02/Program.cs
+++ assOOP02/Program.cs
@@ -4,21 +4,85 @@ using System.Reflection.Emit;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Xml.Linq;
+using static company;
 
+
+#region Q1
+public class company
+{
+    public enum securityy
+    {
+        guest,
+        Developer,
+        secretary,
+        DBA,
+    }
+    public class employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    
+    char gender;
+    public char Gender
+    {
+        get
+        { return gender; }
+
+        set
+        {
+            if (gender == 'M' || gender == 'F')
+                gender = value;
+            else throw new Exception("must be F or M ");
+        }
+
+    }
+    public employee(int id, string Name ,char Gender)
+    {
+            id = id;
+            name = Name;
+            gender = Gender;
+           
+
+    }
+}
+}
+
+#endregion
+
+#region Q2
+public class Date
+{
+
+    public int Day { get; set; }
+    public int Month { get; set; }
+    public int Year { get; set; }
+
+    // Constructor
+    public Date(int day, int month, int year)
+    {
+
+
+        Day = day;
+        Month = month;
+        Year = year;
+    }
+}
+#endregion
 namespace assOOP02
 {
     internal class Program
     {
         static void Main()
         {
-// Design and implement a Class for the employees in a company:
-//Notes:
-//Employee is identified by an ID, Name, security level, salary, hire date and Gender.
-//We need to restrict the Gender field to be only M or F[Male or Female]
-//Assign the following security privileges to the employee(guest, Developer, secretary and DBA) in a form of Enum.
-//We want to provide the Employee Class to represent Employee data in a string Form(override ToString()), display
-//employee salary in a currency format. [Use String.Format() Function].
+            #region Q1
+            employee emp = new employee(1, "alaa", 'f');
+            Console.WriteLine(emp.ToString());
+            #endregion
+            #region Q2
+            Date hiringDate = new Date(10, 7, 2025);
+            Console.WriteLine( hiringDate);
+            #endregion
 
         }
-}
+    }
 }
